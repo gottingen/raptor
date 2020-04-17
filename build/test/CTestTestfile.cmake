4c5c7bb5dc39e21467fcbfb675d110668950cacf
lijippy@163.com <lijippy@163.com>
2020-04-17 15:07:25 +0800

    delete build

--- build/test/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/liyinbin/github/gottingen/raptor/test
-# Build directory: /Users/liyinbin/github/gottingen/raptor/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
